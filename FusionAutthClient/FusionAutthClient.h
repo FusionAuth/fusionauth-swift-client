d738ef424fd724dece503486fb744405cbb576fd
Daniel DeGroff <daniel@inversoft.com>
2018-11-16 19:09:45 -0700

    initial commit

---
+++ FusionAutthClient/FusionAutthClient.h
@@ -0,0 +1,19 @@
+//
+//  FusionAutthClient.h
+//  FusionAutthClient
+//
+//  Created by Daniel DeGroff on 11/16/18.
+//  Copyright © 2018 FusionAuth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FusionAutthClient.
+FOUNDATION_EXPORT double FusionAutthClientVersionNumber;
+
+//! Project version string for FusionAutthClient.
+FOUNDATION_EXPORT const unsigned char FusionAutthClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FusionAutthClient/PublicHeader.h>
+
+
